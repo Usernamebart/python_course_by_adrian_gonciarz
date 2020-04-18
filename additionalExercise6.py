70f5d00ff2cc4ae7beb156614d73e0133b166866
Usernamebart <60666658+Usernamebart@users.noreply.github.com>
2020-04-18 10:03:11 +0200

    Delete additionalExercise6.py

--- additionalExercise6.py
+++
@@ -1,8 +0,0 @@
-kilometry = 12.25
-mile = 7.38
-
-mile_na_kilometry =  mile * 1.61
-kilometry_na_mile =  kilometry / 1.61
-
-print(mile, "mil to", round(mile_na_kilometry, 2), "kilometrów")
-print(kilometry, "kilometrów to", round(kilometry_na_mile,2), "mil")
