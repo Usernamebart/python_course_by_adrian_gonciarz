cd815824af21008a7f904ad72630212f03d87704
Usernamebart <60666658+Usernamebart@users.noreply.github.com>
2020-04-29 19:38:25 +0200

    Update script2.py

--- script2.py
+++ script2.py
@@ -1,58 +1,58 @@
-# description = 'My name is Bart. I work as a tester'
-# greeting = "Hello Friend!"
-#
-# print('Moja ulubiona książka to "Wzgórze psów"')
-# print("W McDonald's kupuje tylko kawę")
-#
-# #Escapowanie stringów poprzez '\'
-# print("Moja ulubiona książka to \"Wzgórze psów\"")
-# print('W McDonald\'s kupuje tylko kawę')
+description = 'My name is Bart. I work as a tester'
+greeting = "Hello Friend!"
 
-# bio1 = 'Nazywam się Bart.'
-# bio2 = 'Pracuję jako tester.'
-#
-# #sklejanie stringów
-# bio = bio1 + bio2
-# print(bio, sep=' ')
+print('Moja ulubiona książka to "Wzgórze psów"')
+print("W McDonald's kupuje tylko kawę")
 
-# login = 'admin1'
-# password = 'haslo2020'
-#
-# print(f'Login: {login}, password: {password}')
-#
-# pi = 3.14
-# print(f'Pole koła o promieniu 2 to {pi * 2**2}')
-#
-# #liczby integer (całkowite)
-# age = 38
-# thousand = 1_000
-# milion = 1_000_000
-#
-# #liczby float (zmiennoprzecinkowe)
-# height = 1.82
-# weight = 74.
-# pi = 3.1415
-# atom_size = 1e-9
-#
-# #typ wartosci zmiennej
-# print(f'Typ wartości przypisanej do zmiennej pi to {type(pi)}')
+#Escapowanie stringów poprzez '\'
+print("Moja ulubiona książka to \"Wzgórze psów\"")
+print('W McDonald\'s kupuje tylko kawę')
+
+bio1 = 'Nazywam się Bart.'
+bio2 = 'Pracuję jako tester.'
+
+#sklejanie stringów
+bio = bio1 + bio2
+print(bio, sep=' ')
+
+login = 'admin1'
+password = 'haslo2020'
+
+print(f'Login: {login}, password: {password}')
+
+pi = 3.14
+print(f'Pole koła o promieniu 2 to {pi * 2**2}')
+
+#liczby integer (całkowite)
+age = 38
+thousand = 1_000
+milion = 1_000_000
+
+#liczby float (zmiennoprzecinkowe)
+height = 1.82
+weight = 74.
+pi = 3.1415
+atom_size = 1e-9
+
+#typ wartosci zmiennej
+print(f'Typ wartości przypisanej do zmiennej pi to {type(pi)}')
 
 # ctrl+d = dubluje linijke w ktorej jestesmy
 
-# h = 3
-# a = 5
-# triangle_area = 1/2 * a * h
-# print(triangle_area)
-#
-# #potęgowanie (w przykłądzie: potęga druga)
-#
-# power_sum = h**2 + a**2
-# print(power_sum)
-#
-# #pierwiastek drugiego stopnia z 3
-#
-# sqrt_3 = 3**0.5
-# print(sqrt_3)
+h = 3
+a = 5
+triangle_area = 1/2 * a * h
+print(triangle_area)
+
+#potęgowanie (w przykłądzie: potęga druga)
+
+power_sum = h**2 + a**2
+print(power_sum)
+
+#pierwiastek drugiego stopnia z 3
+
+sqrt_3 = 3**0.5
+print(sqrt_3)
 
 #boolean - Treu / False
 
@@ -63,7 +63,6 @@ is_day = False
 #iloczyn logiczny and
 is_adult_boy = is_adult and is_boy
 
-#
 is_boy_or_day = is_boy or is_day
 
 age = 20
@@ -77,4 +76,3 @@ my_phone_int = int(my_phone)
 #zamiana int na string
 lenght = 666
 lenght_string = str(lenght)
-
